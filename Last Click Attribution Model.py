e8fe91bd19a14c40692308075721630cfe13ad17
cdevairakkam7 <54795122+cdevairakkam7@users.noreply.github.com>
2020-06-05 11:21:39 -0700

    Update Last Click Attribution Model.py

--- Last Click Attribution Model.py
+++ Last Click Attribution Model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # loading sessions csv using pandas - indexing the most recent rank 
-# Download the source ccsv from the dropbox link
+# Download the source csv from the dropbox link
 sessions=pd.read_csv('https://www.dropbox.com/s/58l1kzbhgi623n4/A1-2_sessions_orders.csv?dl=0')
 idx=sessions.groupby('unique_visitor_id')['visitor_session_rank'].transform(max) == sessions['visitor_session_rank']
 last_click_attribution=sessions[idx]
